01ee7ec58bd62d821e095219ea1980d4409e6bae
Youngjo Gil <dudwh921112@naver.com>
2023-05-15 20:49:00 +0900

    feat: 로그인 유효성 검사 반영

--- src/main/java/com/matching/member/dto/SignInRequest.java
+++ src/main/java/com/matching/member/dto/SignInRequest.java
@@ -4,10 +4,15 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 @Setter
 @Builder
 public class SignInRequest {
+    @NotEmpty(message = "이메일 입력은 필수입니다.")
     private String email;
+
+    @NotEmpty(message = "비밀번호 입력은 필수입니다.")
     private String password;
 }
