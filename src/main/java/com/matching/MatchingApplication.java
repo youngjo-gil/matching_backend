da738291f4a2c881729efc3d71b6bd448fea9820
gilyj <0joofficial@naver.com>
2023-05-15 21:12:14 +0900

    feat: swagger 적용

--- src/main/java/com/matching/MatchingApplication.java
+++ src/main/java/com/matching/MatchingApplication.java
@@ -3,9 +3,11 @@ package com.matching;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableSwagger2
 public class MatchingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MatchingApplication.class, args);
