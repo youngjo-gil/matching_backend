b6e6057bc0cf2c7c247dbb8af6a5403ad40805ff
gilyj <0joofficial@naver.com>
2023-05-17 21:54:30 +0900

    test: test 코드 셋팅

--- src/test/java/com/matching/member/controller/MemberControllerTest.java
+++ src/test/java/com/matching/member/controller/MemberControllerTest.java
@@ -1,77 +1,45 @@
-//package com.matching.member.controller;
-//
-//import com.matching.member.dto.MemberResponse;
-//import com.matching.member.dto.SignInRequest;
-//import com.matching.member.dto.SignUpRequest;
-//import com.matching.member.service.MemberService;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.util.Assert;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//@SpringBootTest
-//class MemberControllerTest {
-//    @Autowired
-//    private MemberService memberService;
-//
-//
-//    @DisplayName("회원가입 성공")
-//    @Test
-//    void signupSuccess() {
-//        // 신규 회원가입
-//        SignUpRequest request = SignUpRequest.builder()
-//                .email("test@tes.com")
-//                .password("1234")
-//                .name("test")
-//                .nickname("test_name")
-//                .build();
-//
-//        boolean signUpComplete = memberService.signup(request);
-//
-//        Assert.isTrue(signUpComplete);
-//    }
-//
-//    @DisplayName("회원가입 실패")
-//    @Test
-//    void signupExistMemberFailure() {
-//        SignUpRequest request = SignUpRequest.builder()
-//                .email("test3@test.com")
-//                .password("1")
-//                .build();
-//
-//        memberService.signup(request);
-//
-//        Assertions.assertThrows(RuntimeException.class, () -> {
-//            memberService.signup(request);
-//        });
-//    }
-//
-//    @DisplayName("로그인 성공")
-//    @Test
-//    void signIn() {
-//        SignInRequest request = SignInRequest.builder()
-//                .email("test@test.com")
-//                .password("1234")
-//                .build();
-//
-//        MemberResponse memberResponse = memberService.signIn(request);
-//        assertEquals(memberResponse.getEmail(), "test@test.com");
-//    }
-//
-//    @DisplayName("로그인 실패")
-//    @Test
-//    void signInPasswordError() {
-//        SignInRequest request = SignInRequest.builder()
-//                .email("test@test.com")
-//                .password("1231")
-//                .build();
-//
-//        Assertions.assertThrows(RuntimeException.class, () -> {
-//            MemberResponse memberResponse = memberService.signIn(request);
-//        });
-//    }
-//}
+package com.matching.member.controller;
+
+import com.matching.common.config.JwtTokenProvider;
+import com.matching.member.service.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@WebMvcTest(MemberController.class)
+@AutoConfigureMockMvc
+class MemberControllerTest {
+    @MockBean
+    private MemberService memberService;
+
+    @MockBean
+    private JwtTokenProvider jwtTokenProvider;
+
+    private static String token = "bearer token";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser
+    @DisplayName("회원가입 성공")
+    void successSignup() throws Exception {
+        // given
+        given(memberService.signup(any(), any())).willReturn(true);
+        // when
+
+        // then
+
+    }
+
+}
