febdae252fae45f2aecc6268818ffb25280a4b2b
Youngjo <dudwh921112@naver.com>
2023-05-19 10:53:32 +0900

    feat: elasticsearch repository 작성

--- src/main/java/com/matching/MatchingApplication.java
+++ src/main/java/com/matching/MatchingApplication.java
@@ -6,13 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
-
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableSwagger2
-@Slf4j
 public class MatchingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MatchingApplication.class, args);
