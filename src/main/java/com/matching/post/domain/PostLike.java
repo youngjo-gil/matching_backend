d38f0939e6b5933e815c24ede543af82a7ae1a80
Youngjo Gil <dudwh921112@naver.com>
2023-06-03 01:41:25 +0900

    feat: 게시글 좋아요 기능

---
+++ src/main/java/com/matching/post/domain/PostLike.java
@@ -0,0 +1,30 @@
+package com.matching.post.domain;
+
+import com.matching.common.domain.BaseEntity;
+import com.matching.member.domain.Member;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.envers.AuditOverride;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@AuditOverride(forClass = BaseEntity.class)
+public class PostLike extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
